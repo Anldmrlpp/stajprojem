27802921ce487261348ac17e94d927586cb0843b
Anldmrlpp <147911313+Anldmrlpp@users.noreply.github.com>
2024-08-05 02:23:23 +0300

    Hata düzeltmeleri yapıldı

--- src/components/UserUpdateProfile.js
+++ src/components/UserUpdateProfile.js
@@ -8,8 +8,8 @@ import profile from '../images/profile.png';
 import { useApiProgress } from '../shared/ApiProgress';
 import { faSave } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { updateUserSuccess } from '../redux/authActions';
 
-import { faSave } from '@fortawesome/free-solid-svg-icons';
 
 const UserUpdateProfile = () => {
 
