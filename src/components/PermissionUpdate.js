4d2da4024a09a803fbc4501675e9dab209d4f995
Anldmrlpp <147911313+Anldmrlpp@users.noreply.github.com>
2024-08-05 15:30:14 +0300

    Backend'ten request dönünceye kadar butonlarda gösterilecek spinnerlar eklendi

--- src/components/PermissionUpdate.js
+++ src/components/PermissionUpdate.js
@@ -7,6 +7,7 @@ import { faSave } from '@fortawesome/free-solid-svg-icons';
 import { useApiProgress } from "../shared/ApiProgress";
 import { ToastContainer, toast } from 'react-toastify';
 import { getPermission, updatePermission } from "../api/apiCalls";
+import Spinner from "./Spinner";
 
 const PermissionUpdate = () => {
 
@@ -16,6 +17,7 @@ const PermissionUpdate = () => {
     const [endDate, setEndDate] = useState();
     const [error, setError] = useState(null);
 
+    const pendingApiCallLoad = useApiProgress('get','/api/v1/permissions/get/');
     const pendingApiCall = useApiProgress('put','/api/v1/permissions');
     const navigate = useNavigate();
     const { id } = useParams();
@@ -32,7 +34,7 @@ const PermissionUpdate = () => {
             setStartDate(response.data.startDate);
             setEndDate(response.data.endDate);
         } catch(error) {
-
+            
         }
     }
 
@@ -74,6 +76,12 @@ const PermissionUpdate = () => {
         }
     }
 
+    if(pendingApiCallLoad) {
+        return(
+            <Spinner />
+        );
+    }
+
     return(
         <div className="container mt-4 mb-4">
             <div className="card text-center h-100 border rounded-3 shadow mb-5">
@@ -89,13 +97,13 @@ const PermissionUpdate = () => {
                     <Input name="employeeId" label="İzin Alan Çalışan Id'si" type="text" onChangeVeriables={onChange} placeholder="3fa85f64-5717-4562-b3fc-2c963f66afa6" value={employeeId} />
 
                     <Input name="description" label="İzin Nedeni" type="text" onChangeVeriables={onChange} placeholder="Hasta, Tatil, Kişisel Nedenler..." value={description} />
-
+                    
                     <Input name="startDate" label="İzin Başlangı Günü" onChangeVeriables={onChange} type="date" value={startDate} />
 
                     <Input name="endDate" label="İzin Bitiş Günü" onChangeVeriables={onChange} type="date" value={endDate} />
 
                     <div>
-
+                                    
                         <button className="btn btn-primary d-inline-flex"
                                 onClick={onClickSave}
                                 disabled = {pendingApiCall}>
